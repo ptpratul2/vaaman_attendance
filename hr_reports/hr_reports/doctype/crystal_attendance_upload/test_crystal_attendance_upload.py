3976cf1454a80420fb771314be32aa3a191ce980
mitalrs <mitalsapkale1999@gmail.com>
2025-08-19 09:12:09 +0530

    doctype crystal attandance upload

---
+++ hr_reports/hr_reports/doctype/crystal_attendance_upload/test_crystal_attendance_upload.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, ms and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCrystalAttendanceUpload(FrappeTestCase):
+	pass
