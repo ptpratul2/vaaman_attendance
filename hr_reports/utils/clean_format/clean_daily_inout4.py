fddd7b38de7a1f95895018147904d92a13548b60
mitalrs <mitalsapkale1999@gmail.com>
2025-09-29 05:59:23 +0000

    added new status code

--- hr_reports/utils/clean_format/clean_daily_inout4.py
+++ hr_reports/utils/clean_format/clean_daily_inout4.py
@@ -192,10 +192,10 @@ def format_timestamp(date_str: str, time_val, is_checkin: bool) -> Optional[str]
 def map_status(raw_status) -> str:
    s = "" if pd.isna(raw_status) else str(raw_status).strip()
    mapping = {
-       "P": "Present", "POW": "Present", "POH": "Present",
+       "P": "Present", "POW": "Present", "POH": "Present", "PWH": "Present",
        "A": "Absent", "A1": "Absent",
        "WO": "Holiday", "H": "Holiday", 
-       "CL": "On Leave", "PL": "On Leave", "SL": "On Leave", "EL": "On Leave", "RL": "On Leave", "LWP": "On Leave", "SDL": "On Leave", "QL": "On Leave", "TU": "On Leave", "CO": "On Leave", "TR": "On Leave", "OH": "On Leave", "ML": "On Leave", "CH": "On Leave",
+       "CL": "On Leave", "PL": "On Leave", "SL": "On Leave", "EL": "On Leave", "RL": "On Leave", "LWP": "On Leave", "SDL": "On Leave", "QL": "On Leave", "TU": "On Leave", "CO": "On Leave", "TR": "On Leave", "OH": "On Leave", "ML": "On Leave", "CH": "On Leave", "SCL": "On Leave", "SPL": "On Leave",
        "MIS": "Half Day",  "HD": "Half Day", "HALF": "Half Day", "HLD": "Half Day",
        "WOH": "Work From Home","WFH": "Work From Home"
    }
