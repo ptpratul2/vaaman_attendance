314286b0ff7efe4d002caf8db27ca66c6d239669
mitalrs <mitalsapkale1999@gmail.com>
2025-11-11 07:21:03 +0000

    fixed the ot report formate for child table

--- hr_reports/hr_reports/doctype/overtime_import/overtime_import.js
+++ hr_reports/hr_reports/doctype/overtime_import/overtime_import.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, ms and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("OverTime Import", {
-// 	refresh(frm) {
+frappe.ui.form.on("OverTime Import", {
+	refresh(frm) {
+		// Set custom formatter for over_time field in child table
+		frm.fields_dict.overtime_import_details.grid.update_docfield_property(
+			'over_time',
+			'formatter',
+			function(value, df, options, doc) {
+				if (value != null && value !== '') {
+					// Format to 2 decimal places
+					return parseFloat(value).toFixed(2);
+				}
+				return value;
+			}
+		);
 
-// 	},
-// });
+		// Refresh the grid to apply formatter
+		frm.fields_dict.overtime_import_details.grid.refresh();
+	}
+});
