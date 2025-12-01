ddd7c32856e7c4e97ff93110938bbef67653eee1
mitalrs <mitalsapkale1999@gmail.com>
2025-12-01 12:02:19 +0000

    added the cleaning script for jspl&jsol tata angul

--- hr_reports/utils/attendance_flow.py
+++ hr_reports/utils/attendance_flow.py
@@ -13,6 +13,7 @@ from hr_reports.utils.clean_format.clean_daily_inout10 import clean_daily_inout1
 from hr_reports.utils.clean_format.clean_daily_inout29 import clean_daily_inout29
 from hr_reports.utils.clean_format.clean_daily_inout7 import clean_daily_inout7
 from hr_reports.utils.clean_format.clean_daily_inout7_1 import clean_daily_inout7_1
+from hr_reports.utils.clean_format.clean_daily_inout7_2 import clean_daily_inout7_2
 from frappe.core.doctype.data_import.data_import import start_import
 
 
@@ -399,6 +400,15 @@ def process_uploaded_file(doc, method):
                 branch=doc.branch
             )
             append_log(doc, "Step 2: Used clean_daily_inout7 for Tata Angul")
+
+        elif doc.branch in ["Jsol Angul", "JSPL Angul Sinter O&M", "Jspl & Jsol angul", "JSPL Angul BF 2 JSOL - VEIL"]:
+            clean_daily_inout7_2(
+                input_path=local_path,
+                output_path=cleaned_path,
+                company=doc.company,
+                branch=doc.branch
+            )
+            append_log(doc, "Step 2: Used clean_daily_inout7 for Tata jspl & jsol Angul")
 
         else:
             clean_crystal_excel(
